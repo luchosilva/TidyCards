35ea0cc35723acc2e29a74e002507378250354a1
luchosilva <44353057+luchosilva@users.noreply.github.com>
2019-05-13 22:30:54 -0400

    Creación clase Card

--- src/tidycards/Card.java
+++ src/tidycards/Card.java
@@ -12,5 +12,31 @@ package tidycards;
 public class Card {
 
   private  String nombre, habilidad;
+  
+  public Card(String nombre, String habilidad) {
+        this.nombre = nombre;
+        this.habilidad = habilidad;
+    }
+
+    public Card() {
+        this.nombre = "";
+        this.habilidad = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public void setHabilidad(String habilidad) {
+        this.habilidad = habilidad;
+    }
 
 }
