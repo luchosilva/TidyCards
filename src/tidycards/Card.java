a7e2f400507e375dd287f70fbc8a09c7494332d4
Oscar Aguayo <43186229+DcOscar@users.noreply.github.com>
2019-05-13 20:20:41 -0400

    Creación diseño solución

---
+++ src/tidycards/Card.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tidycards;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Card {
+
+  private  String nombre, habilidad;
+
+}
